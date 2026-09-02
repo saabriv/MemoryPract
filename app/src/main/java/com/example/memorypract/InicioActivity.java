de056972a29b2a175533165c1642d0714daefa6a
Juancet0 <amarillajuan517@gmail.com>
2026-09-02 10:29:02 -0300

    Se añadio la pantalla de seleccion de colores, y la funcion de boton de colores de la pantalla principal lleva a esta nueva pantalla

--- app/src/main/java/com/example/memorypract/InicioActivity.java
+++ app/src/main/java/com/example/memorypract/InicioActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
 
 public class InicioActivity extends AppCompatActivity {
 
@@ -14,11 +15,16 @@ public class InicioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_inicio);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        findViewById(R.id.btnColores).setOnClickListener(v -> {
+            Intent intent = new Intent(InicioActivity.this, ColoresActivity.class);
+            startActivity(intent);
         });
     }
 }
