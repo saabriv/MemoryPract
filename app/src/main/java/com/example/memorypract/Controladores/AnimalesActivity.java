eda7e1a4e3b4a3d51acc3643c919e6002f4939ef
Juancet0 <amarillajuan517@gmail.com>
2026-09-11 20:57:08 -0300

    Se corrigio el la seleccion de secciones del nav, ahora cuando estas en seccion Animales ya no aparece para seleccionar animales devueta. El boton de VOLVER ahora funciona en animales y colores donde al precionarlo envia directamente a la pantalla principal, no a la pantalla anterior. La pantalla de colores ahora tiene funcion, los colores estan cargados en un array cada vez que se seleccione el color correcto el indice del array se suma y pasa al siguiente color, al terminar el array se muestra una notificacion que acerto todos los colores, esto se va a mostrar de una forma mas visual ya que se añadio bordes en los recuadros de los colores indicando el estado correcto o incorrecto

--- app/src/main/java/com/example/memorypract/Controladores/AnimalesActivity.java
+++ app/src/main/java/com/example/memorypract/Controladores/AnimalesActivity.java
@@ -1,13 +1,15 @@
-package com.example.memorypract;
+package com.example.memorypract.Controladores;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.memorypract.R;
 
 public class AnimalesActivity extends AppCompatActivity {
 
@@ -22,11 +24,32 @@ public class AnimalesActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        findViewById(R.id.btnBack).setOnClickListener(v -> finish());
+        findViewById(R.id.btnBack).setOnClickListener(v -> {
+            Intent intent = new Intent(AnimalesActivity.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+        });
         findViewById(R.id.btnColores).setOnClickListener(v -> {
             Intent intent = new Intent(AnimalesActivity.this, ColoresActivity.class);
             startActivity(intent);
         });
     }
+/*
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // La actividad está a punto de hacerse visible para el usuario
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // La actividad pierde el foco (por ejemplo, se abre un diálogo o se minimiza parcialmente)
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // La actividad va a ser destruida por completo (liberación de memoria)
+    }*/
 }
--- app/src/main/java/com/example/memorypract/Controladores/InicioActivity.java
+++ app/src/main/java/com/example/memorypract/Controladores/InicioActivity.java
@@ -1,4 +1,4 @@
-package com.example.memorypract;
+package com.example.memorypract.Controladores;
 
 import android.os.Bundle;
 
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.content.Intent;
+
+import com.example.memorypract.R;
 
 public class InicioActivity extends AppCompatActivity {
 
